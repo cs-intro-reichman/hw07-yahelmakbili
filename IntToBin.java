1ea5f7793def1e288abeb2da7aa99cbb2a771715
yahelmakbili <yahel.makbili@post.runi.ac.il>
2025-12-27 18:40:49 +0200

    submit

--- IntToBin.java
+++ IntToBin.java
@@ -3,13 +3,9 @@ public class IntToBin {
 
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
-        System.out.println("Binary representation of " + x + " is: " + toBinary(x));
+        System.out.println(toBinary(x));
     }
 
-    /**
-     * Returns the binary representation of the given integer, as a string of 0's
-     * and 1's.
-     */
     public static String toBinary(int x) {
         if (x == 0) {
             return "0";
